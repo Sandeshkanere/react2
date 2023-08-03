24ff183f8abc84b67c09ce6d6f67d477fb10f3f2
sandeshkanere <sandeshkanere@gmail.com>
2023-08-03 18:11:17 +0530

    my first commit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Components */}
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
