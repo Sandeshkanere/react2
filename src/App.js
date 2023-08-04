995ced99053cb63677e4c0ab610937bc616e8e57
sandeshkanere <sandeshkanere@gmail.com>
2023-08-04 10:36:36 +0530

    Created Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       {/* Components */}
       <Navbar />
+      <Home/>
     </div>
   );
 }
